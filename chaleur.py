dd1a56cc6d7f6c743a19e8655c6453f141aacc69
Mohamed Nadir Bakadour <mohamed-nadir.bakadour@etu.mines-paristech.fr>
2023-01-27 10:23:43 +0100

    eul exp2

--- chaleur.py
+++ chaleur.py
@@ -1,11 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-delx=0.1
-dely=0.1
-delt=0.1
+delx=0.01
+dely=0.01
+delt=0.01
+Nx,Ny=int(1/delx),int(1/dely)
 
-X=np.linspace(0,1,int(1/delx +1))
-Y=np.linspace(0,1,int(1/dely +1))
+X=np.linspace(0,1,int(1/delx ))
+Y=np.linspace(0,1,int(1/dely ))
 
 def f(x,y):
     return 0.5 +np.sin(2*np.pi*x)*np.sin(2*np.pi*y) -0.5*np.cos(2*np.pi*x)*np.cos(2*np.pi*y)
@@ -17,7 +19,14 @@ for x in X:
 T=np.array(T)
 
 MatT=np.array([T])
-K=np.array([])
+
+K=np.zeros((Nx*Ny,Nx*Ny))
+for i in range(1,Nx-1):
+    for j in range(1,Ny-1):
+            K[i*Ny +j,(i+1)*Ny+j] = 1/delx**2
+            K[i*Ny +j, i*Ny +j]=-2*(1/delx**2 + 1/dely**2)
+            K[i*Ny +j,(i-1)*Ny+j]=1/delx**2
+            K[i*Ny +j,i*Ny +j-1]=1/dely**2
 
 def fexpli(t,vec):
     return vec-delt*np.dot(K,vec)
@@ -43,3 +52,8 @@ def solve_euler_explicit(f, x0, dt, t0, tf):
     return Ttime,np.array(list(map(aux_euler_solve({0:x0},f,t0,dt),range(len(Ttime)))))
 
 
+ti,mat=solve_euler_explicit(fexpli,T,delt,0,0.5)
+
+plt.imshow(mat[3].reshape(Nx,Ny))
+
+plt.show()
